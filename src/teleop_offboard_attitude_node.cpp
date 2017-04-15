a14f425bf0b68921abafb404579c6ac9eff2ff2f
Kye Morton <kyemorton@hotmail.com>
2017-04-15 18:14:49 +1000

    First functional attempt

--- src/teleop_offboard_attitude_node.cpp
+++ src/teleop_offboard_attitude_node.cpp
@@ -1,6 +1,9 @@
 #include <ros/ros.h>
 #include <sensor_msgs/Joy.h>
 #include <mavros_msgs/AttitudeTarget.h>
+#include <tf2/LinearMath/Quaternion.h>
+
+#include <string.h>
 
 class TeleopAttitude {
 	public:
@@ -12,31 +15,58 @@ class TeleopAttitude {
 	private:
 	  ros::NodeHandle nh_;
 
-	  int linear_, angular_;
-	  double l_scale_, a_scale_;
+	  int param_axis_roll_, param_axis_pitch_, param_axis_yaw_r_, param_axis_throttle_;
+	  double param_scale_a, param_scale_r_;
+	  
+	  std::string param_joy_input;
+	  
 	  ros::Publisher att_pub_;
 	  ros::Subscriber joy_sub_;
 };
 
 TeleopAttitude::TeleopAttitude():
-	linear_(1),
-	angular_(2) {
+	param_axis_roll_(1),
+	param_axis_pitch_(2),
+	param_axis_yaw_r_(3),
+	param_axis_throttle_(4),
+	param_joy_input("/joy") {
+	
+	ROS_INFO("[Teleop] Starting...");
 
-	nh_.param("axis_linear", linear_, linear_);
-	nh_.param("axis_angular", angular_, angular_);
-	nh_.param("scale_angular", a_scale_, a_scale_);
-	nh_.param("scale_linear", l_scale_, l_scale_);
+	nh_.param("axis/roll", param_axis_roll_, param_axis_roll_);
+	nh_.param("axis/pitch", param_axis_pitch_, param_axis_pitch_);
+	nh_.param("axis/yaw_r", param_axis_yaw_r_, param_axis_yaw_r_);
+	nh_.param("axis/throttle", param_axis_throttle_, param_axis_throttle_);
+	
+	nh_.param("scale/angular", param_scale_a, param_scale_a);	//Effectively sets the max roll/pitch angles (rad)
+	nh_.param("scale/rate", param_scale_r_, param_scale_r_);	//Sets how quickly yaw will rotate (rad/s)
 
 	att_pub_ = nh_.advertise<mavros_msgs::AttitudeTarget>("setpoint_raw/attitude", 10);
 
-	joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &TeleopAttitude::joyCallback, this);
+	joy_sub_ = nh_.subscribe<sensor_msgs::Joy>(param_joy_input, 10, &TeleopAttitude::joyCallback, this);
 
+	ROS_INFO("[Teleop] Listening to: %s", param_joy_input.c_str());
 }
 
 void TeleopAttitude::joyCallback(const sensor_msgs::Joy::ConstPtr& joy) {
 	mavros_msgs::AttitudeTarget sp_att;
-	//twist.angular.z = a_scale_*joy->axes[angular_];
-	//twist.linear.x = l_scale_*joy->axes[linear_];
+	tf2::Quaternion sp_q;
+	
+	double roll = param_scale_a*joy->axes[param_axis_roll_];
+	double pitch = param_scale_a*joy->axes[param_axis_pitch_];
+	sp_q.setRPY(roll, pitch, 0.0f);
+	
+	sp_att.orientation.w = sp_q.getW();
+	sp_att.orientation.x = sp_q.getX();
+	sp_att.orientation.y = sp_q.getY();
+	sp_att.orientation.z = sp_q.getZ();
+	
+	sp_att.body_rate.z = param_scale_r_*joy->axes[param_axis_yaw_r_];
+	
+	sp_att.thrust = joy->axes[param_axis_throttle_];
+	
+	sp_att.type_mask |= sp_att.IGNORE_ROLL_RATE | sp_att.IGNORE_PITCH_RATE;
+	
 	att_pub_.publish(sp_att);
 }
 
